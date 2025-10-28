e826f2e4b263631905284f7a9c136509239ee300
Aashu Prakash <156689800+aashuprakash0@users.noreply.github.com>
2025-10-28 23:00:30 +0530

    Add files via upload

---
+++ Project/BankManagementSysem/src/bankmanagementsystem/SignupOne.java
@@ -0,0 +1,15 @@
+package bankmanagementsystem;
+
+import javax.swing.JFrame;
+
+public class SignupOne extends JFrame {
+	 public SignupOne() {
+		setSize(850,800);
+		setLocation(350,200);
+		setVisible(true);
+	}
+	
+public static void main(String[] args) {
+	new SignupOne();
+}
+}
